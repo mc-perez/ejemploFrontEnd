54e929a5c8c5a152a7d2360079a5213634e35056
mc-perez <mauriciocperez@gmail.com>
2023-06-22 23:55:36 -0300

    Ejemplo carga de datos y validacion

---
+++ formSuscripcion/code.js
@@ -0,0 +1,12 @@
+
+const nombre = document.getElementById("name")
+const mail = document.getElementById("email")
+
+function SendInfo() {
+
+    console.log("Datos enviados!")
+
+    console.log("Nombre : ", nombre.value)
+    console.log("Email  :", mail.value)
+
+}
